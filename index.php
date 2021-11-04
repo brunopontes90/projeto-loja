f5a20451cf0a6baf23ee7047f2ce8d13afeb61ad
Bruno Pontes <bruno_confessor@hotmail.com>
2021-11-04 20:04:54 -0300

    Inciio da pagina index

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/8455a3d02b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Loja Virtual</title>
+</head>
+<body class="container">
+    <header class="continer-fluid" id="topo">
+            <!--LOGO-->
+            <nav class="navbar navbar-expand-sm navabr-ligth">
+                <a href="">
+                    <i class="fas fa-gamepad" id="logoGames"></i>
+                    <h1 class="d-none">Site de Jogos</h1>
+                </a>
+
+                <!--Menu COLLAPSE-->
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navBarButton">
+                    <i class="fas fa-bars fa-lg"></i>
+                </button>
+                <div id="navBarButton" class="collapse navbar-collapse">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item text-uppercase font-weight-bold">
+                            <a class="nav-link" href="#jogos" class="nav-link">Comprar Jogos</a>
+                        </li>
+                        <li class="nav-item text-uppercase font-weight-bold">
+                            <a class="nav-link" href="#quemSomos" class="nav-link">Sobre</a>
+                        </li>
+                        <li class="nav-item text-uppercase font-weight-bold">
+                            <a class="nav-link" href="#fale" class="nav-link">Fale Conosco</a>
+
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <section class="jumbotron"></section>
+                <section>
+                    <h1 class="h3 mt-5 text-uppercase text-muted text-center font-weight-bold">Seja Bem Vindo a Nossa Loja!</h1>
+                </section>
+            
+        </main>
+
+        <footer></footer>
+</body>
+</html>
