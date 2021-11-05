3404430916fde47c587b03c341a951776d182214
Bruno Pontes <bruno_confessor@hotmail.com>
2021-11-04 21:29:22 -0300

    Finalização da pagina inicial

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
     <link rel="stylesheet" href="./css/index.css">
     <title>Loja Virtual</title>
 </head>
-<body class="container">
+<body class="container-fluid bg-light">
     <header class="continer-fluid" id="topo">
             <!--LOGO-->
             <nav class="navbar navbar-expand-sm navabr-ligth">
-                <a href="">
+                <a href="index.php">
                     <i class="fas fa-gamepad" id="logoGames"></i>
                     <h1 class="d-none">Site de Jogos</h1>
                 </a>
@@ -28,7 +28,7 @@
                 <div id="navBarButton" class="collapse navbar-collapse">
                     <ul class="navbar-nav mx-auto">
                         <li class="nav-item text-uppercase font-weight-bold">
-                            <a class="nav-link" href="#jogos" class="nav-link">Comprar Jogos</a>
+                            <a class="nav-link" href="#jogos" class="nav-link">Ver Jogos</a>
                         </li>
                         <li class="nav-item text-uppercase font-weight-bold">
                             <a class="nav-link" href="#quemSomos" class="nav-link">Sobre</a>
@@ -46,11 +46,63 @@
         <main>
             <section class="jumbotron"></section>
                 <section>
-                    <h1 class="h3 mt-5 text-uppercase text-muted text-center font-weight-bold">Seja Bem Vindo a Nossa Loja!</h1>
+                    <h1 class="h3 mt-5 text-uppercase text-muted text-center font-weight-bold">História do Videogame</h1>
                 </section>
+
+                <article class="container mt-5">
+                    <p>O Videogame ou game é uma disputa competitiva, executada no formato de um jogo eletrônico. O jogador permanece diante de um veículo que transmite as imagens do confronto lúdico, quase sempre uma TV ou o dispositivo de um computador. Normalmente estende-se este termo também para definir o suporte no qual os games são processados, conhecido como console.</p>
+                    <p>Ao que parece, o físico William Higinbotham, integrante do projeto Manhattan, responsável pela produção da primeira bomba atômica, foi o criador do Videogame, em 1958, mas inconsciente da importância de sua obra-prima ele não se preocupou em patenteá-la, portanto não foi oficialmente considerado seu artífice.</p>
+                    <p>No auge da Guerra Fria, ele elaborou um mecanismo que incrementasse o número de visitantes do Brookhaven National Laboratories, sediado em Nova York, pois o governo norte-americano tinha como intenção exibir seu potencial nuclear diante de seu povo. Desta forma ele produziu um jogo de tênis com apenas dois traços primitivos e uma bola, batizando-o de Tennis Programming ou Tennis for Two, exposto em uma tela de 15 polegadas e projetado para ser processado em um computador analógico.</p>
+                    <p>No auge da Guerra Fria, ele elaborou um mecanismo que incrementasse o número de visitantes do Brookhaven National Laboratories, sediado em Nova York, pois o governo norte-americano tinha como intenção exibir seu potencial nuclear diante de seu povo. Desta forma ele produziu um jogo de tênis com apenas dois traços primitivos e uma bola, batizando-o de Tennis Programming ou Tennis for Two, exposto em uma tela de 15 polegadas e projetado para ser processado em um computador analógico.</p>
+                    <p>Do primitivo videogame de Higinbotham, o game evoluiu da simulação de jogos convencionais, tais como os de damas e xadrez, para disputas e confrontos expositivos.</p>
+                    <p>Em 1966, o engenheiro eletrônico Ralph Baer, alemão refugiado nos Estados Unidos durante a Segunda Guerra Mundial, considerado hoje o pai dos consoles de games, vislumbrou a oportunidade de criar um equipamento que processasse jogos eletrônicos por meio de sua veiculação na televisão, com poucos recursos financeiros.</p>
+                    <p>Isto ocorreu quando Baer trabalhava para a Sander Associates. Contando com a contribuição de seus amigos e companheiros de trabalho, o primeiro fruto de suas experiências foi o ‘chasing game’, elaborado em 1967, um rústico game de Ping Pong, no qual duas figuras no formato quadrado podiam ser manuseadas pelo competidor, e assim deslocadas ao longo da tela.</p>
+                    <p>Seu inventor o patenteou logo em seguida e, um ano depois, expôs o primeiro modelo do videogame, designado de Brown Box, o qual consistia de disputas de futebol, vôlei e tiro. Portanto, não foi o modelo Atari o primeiro game a ser comercializado no mercado de videogames, mas sim este ancestral dos games.</p>
+                    <p>A Magnavox, uma filial da Philips sediada na Holanda, imediatamente se dispôs a lançar para o público consumidor o primeiro console de videogame, intitulado Odyssey 100. Deste primeiro console aos Nintendos, PlayStations e Xbox de hoje, este filão produtivo converteu-se, em princípios do século XXI, em uma empresa extremamente rendosa, captando assim investimentos de monta, que lhe permitem competir com a esfera cinematográfica e até mesmo transcender seus lucros.</p>
+                </article>
             
         </main>
 
-        <footer></footer>
+        <footer class="mt-5">
+            <nav id="redes " class="row">
+                <div class="col-sm-12 ">
+                    <div>
+                        <ul class="nav justify-content-center ">
+
+                            <!--FACEBOOK-->
+                            <li class="nav-item mr-4">
+                                <a href="https://www.facebook.com/" target="_blank">
+                                    <i class="fab fa-facebook-f text-decoration-none"></i>
+                                </a>
+                            </li>
+
+                            <!--INSTAGRAM-->
+                            <li class="nav-item mr-4">
+                                <a href="https://www.instagram.com/" target="_blank">
+                                    <i class="fab fa-instagram text-decoration-none"></i>
+                                </a>
+                            </li>
+
+                            <!--WHATSAPP-->
+                            <li class="nav-item mr-4">
+                                <a href="https://web.whatsapp.com/" target="_blank">
+                                    <i class="fab fa-whatsapp text-decoration-none"></i>
+                                </a>
+                            </li>
+
+                            <!--MARCAR REGISTRADA-->
+                            <div class="col-sm-12">
+                                <div class="nav justify-content-center">
+                                    <li class="nav-item">
+                                        <i id="marca" class="far fa-registered"> 2021 - Loja Virtual</i>
+                                    </li>
+                                </div>
+                            </div>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </footer>
 </body>
 </html>
